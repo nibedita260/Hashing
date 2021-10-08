185df62181894010c9efdfb7271afc68e375f720
nibedita260 <deepi.nibedita260.com>
2021-10-08 21:07:05 +0530

    [Nibedita] Add: Frequency of WordsIn Sentence Hashing Problem Features Implemented

--- Hashing/Program.cs
+++ Hashing/Program.cs
@@ -8,14 +8,30 @@ namespace Hashing
         {
             Console.WriteLine("Hashing table");
             MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            hash.Add("0", "To");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
-            string hash5 = hash.Get("5");
-            Console.WriteLine("5th index value: "+hash5);
+            bool isExit = false;
+            int options;
+            while (!isExit)
+            {
+                Console.WriteLine("Choose 1.FrequencyOfWordsInSentence");
+                options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        hash.Add("0", "To");
+                        hash.Add("1", "be");
+                        hash.Add("2", "or");
+                        hash.Add("3", "not");
+                        hash.Add("4", "to");
+                        hash.Add("5", "be");
+                        string hash5 = hash.Get("5");
+                        Console.WriteLine("5th index value: " + hash5);
+                        break;
+                    default:
+                        Console.WriteLine("Choose valid options");
+                        break;
+                }
+            }
+           
 
         }
     }
